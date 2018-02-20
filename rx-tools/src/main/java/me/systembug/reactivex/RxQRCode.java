68f95e4c656ef4ee602b3e3f3e78e8ed962da538
Albert <albert.li@salesforce.com>
2018-02-20 10:56:41 -0800

    use comonutils

--- rx-tools/src/main/java/me/systembug/reactivex/RxQRCode.java
+++ rx-tools/src/main/java/me/systembug/reactivex/RxQRCode.java
@@ -7,19 +7,17 @@ import cn.bingoogolapple.qrcode.zxing.QRCodeEncoder;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
 
+import static me.systembug.utils.Screens.dip2px;
+
 /**
  * Created by albert on 9/21/16.
  */
 public class RxQRCode {
     public static Single<Bitmap> createQRCode(String content, Context context, int size) {
         return Single.create(emitter -> {
-                    Bitmap image = QRCodeEncoder.syncEncodeQRCode(content, dp2px(context, size));
+                    Bitmap image = QRCodeEncoder.syncEncodeQRCode(content, dip2px(context, size));
                     emitter.onSuccess(image);
                 });
     }
 
-    private static int dp2px(Context context, int dip){
-        float scale = context.getResources().getDisplayMetrics().density;
-        return (int)(dip * scale + 0.5f);
-    }
 }
