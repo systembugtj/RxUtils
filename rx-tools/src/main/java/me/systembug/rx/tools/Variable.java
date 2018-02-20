a677472850c412432713687154a6780850b24677
Albert <albert.li@salesforce.com>
2018-02-20 09:41:05 -0800

    Bump to latest, add Variable and test

---
+++ rx-tools/src/main/java/me/systembug/rx/tools/Variable.java
@@ -0,0 +1,32 @@
+package me.systembug.rx.tools;
+
+/**
+ * Created by albert.li on 2/16/18.
+ */
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.BehaviorSubject;
+
+public class Variable<T> {
+    private T value;
+
+    private final BehaviorSubject<T> subject;
+
+    public Variable(T value) {
+        this.value = value;
+        subject =  BehaviorSubject.create();
+    }
+
+    public synchronized T get() {
+        return value;
+    }
+
+    public synchronized void set(T value) {
+        this.value = value;
+        subject.onNext(this.value);
+    }
+
+    public Observable<T> asObservable() {
+        return subject.toSerialized();
+    }
+}
---
+++ rx-tools/src/test/java/me/systembug/rx/tools/VariableTest.java
@@ -0,0 +1,45 @@
+package me.systembug.rx.tools;
+
+import org.junit.Test;
+
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.TestScheduler;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class VariableTest {
+    @Test
+    public void intValueChange() {
+
+
+        Variable<Integer> at = new Variable<Integer>(10);
+
+        at
+                .asObservable()
+                .subscribe(value -> {
+                    assertEquals(5, value.intValue());
+                });
+
+        at.set(5);
+    }
+
+    @Test
+    public void stringValueChange() {
+
+
+        Variable<String> at = new Variable<String>("A");
+
+        at
+                .asObservable()
+                .subscribe(value -> {
+                    assertEquals("B", value.toString());
+                });
+
+        at.set("B");
+    }
+}
