875c5fe8d92f5d84dc6da2ff41e545276271b6af
Albert Li <albert.li@salesforce.com>
2018-12-06 11:28:25 -0800

    androidX

--- app/src/main/java/me/systembug/rx/rxutils/MainActivity.java
+++ app/src/main/java/me/systembug/rx/rxutils/MainActivity.java
@@ -1,10 +1,10 @@
 package me.systembug.rx.rxutils;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +12,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding3.view.RxView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
